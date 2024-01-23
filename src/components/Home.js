0c5ff875f1d74f9bc3e782f486f305389a441f91
ShreeramNew <shreerambca1@gmail.com>
2024-01-23 21:58:52 +0530

    logingOff- today's last commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
-import BatteryImage from "../images/Battery.png"
+import charging from "../images/Battery.png";
+import notCharging from "../images/Unplug.png";
+import lowBattery from "../images/LowBattery.svg";
+import unknownStatus from "../images/Unknown.svg"
 export default function Home() {
    const [batteryStatus, setBatteryStatus] = useState(null);
    const [batteryIsCharging, setBatteryIsCharging] = useState(false);
@@ -34,11 +37,20 @@ export default function Home() {
       <>
          <div className="flex justify-center items-center h-screen border-black border-2 h-10">
             <div className="flex gap-x-10 flex-row justify-center align-middle bg-blue-200 text-white border-black border-2 w-1/2">
-               <div className="border-black border-2 p- m-10 w-1/4 h-1/4 bg-red-500">
-                  <img src={BatteryImage} className=" bg-white" alt="" />
+               <div className="border-black border-2 m-10 w-1/4 min-h-1/4 bg-red-500">
+                  <img
+                     src={batteryIsCharging ? charging : notCharging}
+                     className=" bg-white"
+                     alt=""
+                  />
                   <h1>Your Battery is {batteryStatus}</h1>
                </div>
-               <div className="border-black border-2 m-10 bg-blue-800">
+               <div className="border-black border-2 m-10 bg-blue-800 w-1/4 min-h-1/4">
+                  <img
+                     src={unknownStatus}
+                     className=" bg-white"
+                     alt=""
+                  />
                   <h1>You Battery Charge Level is {batteryLevel}%</h1>
                </div>
             </div>
