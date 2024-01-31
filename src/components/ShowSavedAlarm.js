e293a53ce7f52439ba430c1aefc3f4cc51eefd6a
ShreeramNew <shreerambca1@gmail.com>
2024-01-31 13:15:00 +0530

    implemented localStorage Setup

--- src/components/ShowSavedAlarm.js
+++ src/components/ShowSavedAlarm.js
@@ -3,16 +3,21 @@ import { dataContext } from "../contexts/DataController";
 import EachSavedAlarm from "./EachSavedAlarm";
 
 export default function ShowSavedAlarm() {
-   let ContextData=useContext(dataContext)
-   let handleClick=()=>{
+   let ContextData = useContext(dataContext);
+   let handleClick = () => {
       ContextData[2](true);
-   }
-   let savedAlarms=JSON.parse(localStorage.getItem('savedAlarms'))
-   console.log(savedAlarms);
+   };
+   let savedAlarms = JSON.parse(localStorage.getItem("savedAlarms"));
    return (
       <div className=" border-black border-2 h-fit max-h-32 mt-10 overflow-y-scroll w-2/4  absolute top-16 left-1/4">
-         <EachSavedAlarm percentage={99}/>
-         <div onClick={handleClick} className=" w-8 h-8 rounded-full sticky bg-blue-950 overflow-hidden border-2 bottom-1 m-auto  border-black">
+         {savedAlarms.map((SavedAlarm) => {
+            return <EachSavedAlarm percentage={SavedAlarm[0]} />;
+         })}
+
+         <div
+            onClick={handleClick}
+            className=" w-8 h-8 rounded-full sticky bg-blue-950 overflow-hidden border-2 bottom-1 m-auto  border-black"
+         >
             <h1 className=" text-center text-zinc-50 scale-150 m-0 pb-1">+</h1>
          </div>
       </div>
--- src/contexts/DataController.js
+++ src/contexts/DataController.js
@@ -4,10 +4,19 @@ import { createContext } from "react";
 export const dataContext = createContext();
 export default function DataController(props) {
    const [showSetAlarm, setShowSetAlarm] = useState(false);
-   const savedAlarms = [];
    const recieveUserInput = (percentage, Path) => {
-      savedAlarms.push([percentage, Path]);
-      localStorage.setItem("savedAlarms",JSON.stringify(savedAlarms) );   
+      let newAlarm=[percentage, Path];
+      let savedAlarms=JSON.parse(localStorage.getItem('savedAlarms'));
+      if(savedAlarms){
+        savedAlarms.push(newAlarm)
+        localStorage.setItem('savedAlarms',JSON.stringify(savedAlarms))
+        console.log(JSON.parse(localStorage.getItem('savedAlarms')));
+      }
+      else{
+         let savedAlarms=[newAlarm];
+         localStorage.setItem('savedAlarms',JSON.stringify(savedAlarms))
+      }
+      
    };
    return (
       <dataContext.Provider value={[recieveUserInput, showSetAlarm, setShowSetAlarm]}>
